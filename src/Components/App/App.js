1a616d808b00a3df9c4d6787a118a85c4b2df63b
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-08-02 18:46:14 -0600

    Display nav component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
-import './App.css';
+import { Route } from 'react-router-dom'
+import './App.css'
+
+
+// Pages
+import Nav from '../../Components/Nav/Nav.js'
 
 class App extends Component {
 	constructor() {
@@ -12,6 +17,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Nav />
 				<header className="App-header">
 					<h1>
 						VeggieLovers
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {
+	return(
+		<nav className='nav-bar'>
+			<p>nav bar</p>
+		</nav>
+	)
+}
+
+export default Nav 
