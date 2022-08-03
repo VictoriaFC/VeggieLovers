67245fc3f48f4e6cf030ba270bee65e3b7d19481
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-08-02 22:17:00 -0600

    Add cursor pointer to nav buttons

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -4,21 +4,21 @@ import './Nav.css'
 
 const Nav = () => {
 	const location = useLocation()
-	
+
 	return(
 		<nav>
-			<Link to='/' className='nav-home'>
-				<h1 className='logo'>VeggieLovers</h1>
-			</Link>
 			{location.pathname !== '/favorites' ? (
 				<Link to='/favorites'>
-					<button>Favorites</button>
+					<button className='nav-favorites'>Favorites</button>
 				</Link>
 			) : (
-				<Link to='/' className='nav-favorites'>
-					<button>Home</button>
+				<Link to='/'>
+					<button className='nav-home'>Home</button>
 				</Link>
 			)}
+			<Link to='/'>
+				<h1 className='veggie-lovers-logo'>VeggieLovers</h1>
+			</Link>
 		</nav>
 	)
 }
