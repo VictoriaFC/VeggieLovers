a2dc679b1dd2eb3517b52baa74379e89fe0ab696
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-08-03 22:42:04 -0600

    Update background color to white and change font for welcome

--- src/Components/MealCard/MealCard.js
+++ src/Components/MealCard/MealCard.js
@@ -6,7 +6,9 @@ const MealCard = ({id, name, description, instructions, ingredients, addMealToFa
 		<div className='meal-card'>
 			<h3>{name}</h3>
 			<p>{description}</p>
+			<h4>Instructions:</h4>
 			<p>{instructions}</p>
+			<h4>Ingredients:</h4>
 			<p>{ingredients}</p>
 			<button onClick={() => addMealToFavorites(id)}>Add to Favorites</button>
 		</div>
