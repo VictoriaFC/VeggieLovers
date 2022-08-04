2bf80a9a899d0a53a53f8fb45ab1f8ea656e3616
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-08-03 21:47:26 -0600

    Remove button classNames and add styling for all buttons in app.css

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -6,7 +6,7 @@ const Welcome = ({updateMeals}) => {
 		<div className='welcome-container'>
 			<h1>Welcome to VeggieLovers</h1>
 			<p>app blurb description goes here</p>
-			<button className='get-random-meals-button' onClick={updateMeals}>get random meals</button>
+			<button onClick={updateMeals}>get random meals</button>
 		</div>
 	)
 }
