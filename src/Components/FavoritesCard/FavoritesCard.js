54107807d8ee9fd4bd51647db4591074fc3c9c6d
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-08-05 21:31:53 -0600

    Add @media screen styling to Favorites Card for desktop and mobile

--- src/Components/FavoritesCard/FavoritesCard.js
+++ src/Components/FavoritesCard/FavoritesCard.js
@@ -4,23 +4,27 @@ import './FavoritesCard.css'
 const FavoritesCard = ({id, name, imageUrl, instructions, ingredients, deleteMealFromFavorites, isFavorite}) => {
 	return (
 		<div className='fav-meal-card'>
-			<div className="fav-meal-card-left">
-				<img className="fav-meal-image"src={imageUrl}></img>
-			</div>
-			<div className="fav-meal-card-middle">
-				<h3 className="fav-meal-card-header" >{name}</h3>
-				<div className="fav-instructions-container">
-					<h4>Instructions:</h4>
-					<p>{instructions}</p>
+			<section className="fav-top-section">
+				<div className="fav-meal-card-left">
+					<img className="fav-meal-image"src={imageUrl}></img>
 				</div>
-			<button onClick={() => deleteMealFromFavorites(id)}>Remove Favorite</button>
-			</div>
-			<div className="fav-meal-card-right">
-				<div className="fav-ingredients-container">
-					<h4>Ingredients:</h4>
-					<p>{ingredients}</p>
+				<div className="fav-meal-card-middle">
+					<h3 className="fav-meal-card-header" >{name}</h3>
+					<div className="fav-instructions-container">
+						<h4>Instructions:</h4>
+						<p>{instructions}</p>
+					</div>
 				</div>
-			</div>
+				<div className="fav-meal-card-right">
+					<div className="fav-ingredients-container">
+						<h4>Ingredients:</h4>
+						<p>{ingredients}</p>
+					</div>
+				</div>
+			</section>
+			<section className="fav-bottom-section">
+				<button onClick={() => deleteMealFromFavorites(id)}>Remove Favorite</button>
+			</section>
 		</div>
 	)
 }
