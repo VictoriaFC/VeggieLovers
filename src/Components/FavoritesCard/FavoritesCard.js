659a3f172e58c17437840e1c3874e38360143d5b
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-08-05 21:24:16 -0600

    Add @media screen styling to MealCard css for desktop and mobile

--- src/Components/FavoritesCard/FavoritesCard.js
+++ src/Components/FavoritesCard/FavoritesCard.js
@@ -3,20 +3,20 @@ import './FavoritesCard.css'
 
 const FavoritesCard = ({id, name, imageUrl, instructions, ingredients, deleteMealFromFavorites, isFavorite}) => {
 	return (
-		<div className='meal-card'>
-			<div className="meal-card-left">
-				<img className="meal-image"src={imageUrl}></img>
+		<div className='fav-meal-card'>
+			<div className="fav-meal-card-left">
+				<img className="fav-meal-image"src={imageUrl}></img>
 			</div>
-			<div className="meal-card-middle">
-				<h3 className="meal-card-header" >{name}</h3>
-				<div className="instructions-container">
+			<div className="fav-meal-card-middle">
+				<h3 className="fav-meal-card-header" >{name}</h3>
+				<div className="fav-instructions-container">
 					<h4>Instructions:</h4>
 					<p>{instructions}</p>
 				</div>
 			<button onClick={() => deleteMealFromFavorites(id)}>Remove Favorite</button>
 			</div>
-			<div className="meal-card-right">
-				<div className="ingredients-container">
+			<div className="fav-meal-card-right">
+				<div className="fav-ingredients-container">
 					<h4>Ingredients:</h4>
 					<p>{ingredients}</p>
 				</div>
